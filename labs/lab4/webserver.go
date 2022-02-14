f25d0f56f4662ff2024d7c741dbd3d0f1a9fb240
Gandhar <gandhard1@gmail.com>
2022-02-14 11:21:43 -0500

    added comments

--- labs/lab4/webserver.go
+++ labs/lab4/webserver.go
@@ -18,47 +18,47 @@ func main() {
 	mux.HandleFunc("/create", db.create)
 	mux.HandleFunc("/update", db.update)
 	mux.HandleFunc("/delete", db.delete)
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+	log.Fatal(http.ListenAndServe("localhost:8000", mux)) // Listens for curl communication of localhost
 }
 
 type dollars float32
 
 func (d dollars) String() string { return fmt.Sprintf("$%.2f", d) }
 
-type database map[string]dollars
+type database map[string]dollars // database of items with their polar prices
 
 func (db database) list(w http.ResponseWriter, req *http.Request) {
-	RWLock.Lock()
+	RWLock.Lock() // Locks in case of multiple reads/writes
 	for item, price := range db {
-		fmt.Fprintf(w, "\n%s: %s", item, price)
+		fmt.Fprintf(w, "\n%s: %s", item, price) //iterates through the database and prints out each item and dollar price
 	}
 	RWLock.Unlock()
 }
 
 func (db database) price(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
-	RWLock.Lock()
-	if price, ok := db[item]; ok {
-		fmt.Fprintf(w, "%s\n", price)
+	item := req.URL.Query().Get("item") //Gets the item from the query
+	RWLock.Lock()                       //locks the system
+	if price, ok := db[item]; ok {      // if the item exists
+		fmt.Fprintf(w, "%s\n", price) // prints price
 		RWLock.Unlock()
 	} else {
-		w.WriteHeader(http.StatusNotFound) // 404
-		fmt.Fprintf(w, "no such item: %q\n", item)
+		w.WriteHeader(http.StatusNotFound)         // 404
+		fmt.Fprintf(w, "no such item: %q\n", item) // if the item does not exist write and error
 		RWLock.Unlock()
 	}
 }
 
 func (db database) create(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
+	item := req.URL.Query().Get("item") // gets item and price
 	//convert string to float and get error status ok
 	priceStr := req.URL.Query().Get("price")
-	priceFloat, ok := strconv.ParseFloat(priceStr, 32)
+	priceFloat, ok := strconv.ParseFloat(priceStr, 32) // converts price into float32, if it cannot, it throws an error
 
 	if ok == nil {
-		if _, ok := db[item]; ok {
+		if _, ok := db[item]; ok { // checks if item alrady exists
 			fmt.Fprintf(w, "Item already exist in list, please use 'update' request")
 		} else {
-			RWLock.Lock()
+			RWLock.Lock() // adds item
 			fmt.Fprintf(w, "Adding item: %s ", item)
 			fmt.Fprintf(w, "\nprice: %f", priceFloat)
 			db[item] = dollars(priceFloat)
@@ -75,10 +75,10 @@ func (db database) update(w http.ResponseWriter, req *http.Request) {
 	//convert string to float and get error status ok
 	priceStr := req.URL.Query().Get("price")
 	priceFloat, ok := strconv.ParseFloat(priceStr, 32)
-	RWLock.Lock()
+	RWLock.Lock() //Works similarly to create, but instead it checks if the item already exists
 	if _, itemExist := db[item]; itemExist {
 		if ok == nil {
-			db[item] = dollars(priceFloat)
+			db[item] = dollars(priceFloat) // changes the price
 			RWLock.Unlock()
 			fmt.Fprintf(w, "updated price of item %s: %s", item, dollars(priceFloat))
 
@@ -93,11 +93,11 @@ func (db database) update(w http.ResponseWriter, req *http.Request) {
 }
 
 func (db database) delete(w http.ResponseWriter, req *http.Request) {
-	item := req.URL.Query().Get("item")
-	RWLock.Lock()
-	if _, itemExist := db[item]; itemExist {
-		delete(db, item)
-		RWLock.Unlock()
+	item := req.URL.Query().Get("item")      //gets the item name
+	RWLock.Lock()                            //locks system
+	if _, itemExist := db[item]; itemExist { //checks if item exists
+		delete(db, item) // deletes the item
+		RWLock.Unlock()  // unlocks
 		fmt.Fprintf(w, "deleted item %s", item)
 	} else {
 		fmt.Fprintf(w, "%s does not exist in list", item)
