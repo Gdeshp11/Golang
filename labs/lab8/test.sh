e911d3d9ff8cec21b8f4bc678ae9b8d52cc4258e
Gandhar <gandhard1@gmail.com>
2022-03-21 10:43:19 -0400

    added lab8

---
+++ labs/lab8/test.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+list()
+{
+    echo "list of items in database: "
+    curl -w "\n" "http://localhost:8000/list"
+}
+
+price()
+{
+    echo "price of item: $1"
+    curl -w "\n" "http://localhost:8000/price?item=$1"
+}
+
+create()
+{
+    echo "adding item: $1 in database of price: $2"
+    curl -w "\n" "http://localhost:8000/create?item=$1&price=$2"
+}
+
+update()
+{
+    echo "updating price of item: $1 with price: $2"
+    curl -w "\n" "http://localhost:8000/update?item=$1&price=$2"
+}
+
+delete()
+{
+    echo "deleting item: $1 from database"
+    curl -w "\n" "http://localhost:8000/delete?item=$1"
+}
+ 
+printf "Enter input--->\nlist\nprice\ncreate\nupdate\ndelete\n"
+read OPTION
+case $OPTION in
+  list)
+    list
+    ;;
+
+  price)
+    read -p "Enter item: " ITEM
+    price $ITEM
+    ;;
+
+  create)
+    read -p "Enter item: " ITEM
+    echo "Enter price of item: "
+    read PRICE
+    create $ITEM $PRICE
+    ;;
+
+  update)
+    read -p "Enter item: " ITEM
+    echo "Enter price of item: "
+    read PRICE
+    update $ITEM $PRICE
+    ;;
+
+    delete)
+    read -p "Enter item: " ITEM
+    delete $ITEM
+    ;;
+
+  *)
+    echo -n "unknown option: $OPTION"
+    ;;
+esac
